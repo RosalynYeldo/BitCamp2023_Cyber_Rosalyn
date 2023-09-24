c2a79e77dc1ac988c00bb03e0628b5cb48cafed4
RosalynYeldo <rosalyn.yeldo@gmail.com>
2023-09-24 20:01:19 +0530

    [m1]

--- bit.py
+++ bit.py
@@ -1,45 +1,41 @@
-import pandas as pd
-
-yo=pd.read_csv(r'C:\Users\laksh\Dropbox\My PC (LAPTOP-6UJV2OF2)\Downloads\users.csv\users.csv')
-
-
-'''def __init__(self, username):
+class User:
+    def __init__(self, username):
         self.username = username
-        self.failed_login_attempts = 0'''
+        self.failed_login_attempts = 0
 
-def increment_failed_login(self):
+    def increment_failed_login(self):
         self.failed_login_attempts += 1
 
-'''def reset_failed_login(self):
-        self.failed_login_attempts = 0'''
-        
-username = input("Enter your username: ")
+    def reset_failed_login(self):
+        self.failed_login_attempts = 0
 
-bunny = yo.to_dict()
+users = {
+    "user1": User("user1"),
+    "user2": User("user2"),
+    # Add more users here
+}
 
-if username in bunny:
-    #user = bunny[username] 
-    exit()
+def detect_identity_threat(username):
+    if username in users:
+        user = users[username] 
+        exit()
         # user.increment_failed_login()
        # if user.failed_login_attempts >= 3:
         #    print(f"Suspicious login attempts detected for user {username}. Please investigate.")
             # You can take further actions here, such as locking the account or notifying the user/administrator.
         #else:
         #print(f"Failed login attempt for user {username}. Attempts: {user.failed_login_attempts}")
-else:
-    user = bunny[username]
-    increment_failed_login()
-    print(f"User {username} not found.")
-    if user.failed_login_attempts >= 3:
-            print(f"Suspicious login attempts detected for user {username}. Please investigate.")
     else:
-        print(username)
+        user.increment_failed_login()
+        print(f"User {username} not found.")
+        if user.failed_login_attempts >= 3:
+            print(f"Suspicious login attempts detected for user {username}. Please investigate.")
         #else:
          #   print(f"Failed login attempt for user {username}. Attempts: {user.failed_login_attempts}")    
-'''
+
 # Consecutive login attempts
 for _ in range(3):
     username = input("Enter your username: ")
     detect_identity_threat(username)
 
-'''
+# Reset failed login attempts for the user
